a358294a145d50f711e395edec0422d47bdb3942
Kent Loog <kentloog@users.noreply.github.com>
2016-10-12 15:28:12 +0300

    Added login.php, functions.php and data.php

---
+++ data.php
@@ -0,0 +1,80 @@
+﻿<?php
+	//ühendan sessiooniga
+	require("functions.php");
+	
+		// kui ei ole sisseloginud, suunan login lehele
+		if(!isset($_SESSION["userId"])) {
+			header("Location: login.php");
+		}
+	
+		//kas aadressi real on logout
+	if (isset($_GET["logout"])) {
+		session_destroy();
+		
+		header("Location: login.php");
+		
+	}
+	if ( isset($_POST["event"]) &&
+	     isset($_POST["date"]) &&
+		 !empty($_POST["event"]) &&
+		 !empty($_POST["date"])
+		 ) {
+			 saveEvent($_POST["event"], $_POST["date"]);
+			 }
+			 
+		$event = getAllEvents();
+		//echo "<pre>";
+		//var_dump($people);
+		//echo "</pre>";
+?>
+
+<h1>Data</h1>
+
+<p>
+	Tere tulemast <?=$_SESSION["userEmail"];?>!
+	<a href="?logout=1">logi välja</a>
+	
+</p>
+<body>
+	
+		<h1>Salvesta andmed</h1>
+		
+		<form method="POST" >
+		
+			<label>ürituse nimi</label><br>
+			<input type="text" name="event" >
+			<br> <br>
+			<label>kuupäev</label><br>
+			<input type="date" name="date">
+			<br> <br>
+			<input type="submit" value="Salvesta">
+		</form>
+
+		
+		<h2>Arhiiv</h2>
+
+<?php
+	$html = "<table>";
+		
+		$html .= "<tr>";
+			$html .= "<th>ID</th>";
+			$html .= "<th>üritus</th>";
+			$html .= "<th>kuupäev</th>";
+		$html .= "</tr>";
+		
+		foreach ($event as $e) {
+			$html .= "<tr>";
+			$html .= "<td>".$p->id."</td>";
+			$html .= "<td>".$p->event."</td>";
+			$html .= "<td>".$p->date."</td>";
+		$html .= "</tr>";
+			
+		}
+	
+	
+	
+	$html .= "</table>";
+	
+	echo $html;
+?>
+
---
+++ functions.php
@@ -0,0 +1,116 @@
+﻿<?php
+	require ("../../config.php");
+	/*function hello($firstname, $lastname) {
+		return "Tere tulemast ".$firstname." ".$lastname."!";
+		
+	}
+	echo hello("Kent", "Loog");
+	echo "<br>";
+	*/
+	// SEE FAIL PEAB OLEMA SIIS SEOTUD KÕIGIGA KUS TAHAME SESSIOONI KASUTADA, SAAB KASUTADA NUUD $_SESSION MUUTUJUAT	
+	
+	session_start();
+	
+	$database = "if16_kentloog";
+	
+	
+	function signup($email, $password, $gender, $birthdate) {
+			
+			
+			$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+			
+			$stmt = $mysqli->prepare("INSERT INTO mvp_user (email, password, gender, birthdate) VALUE (?, ?, ?, ?)");
+			echo $mysqli->error;
+			
+			$stmt->bind_param("ssss", $email, $password, $gender, $_POST["birthdate"]);
+			
+			if ($stmt->execute() ){
+				echo "õnnestus";
+			} else {
+				echo "ERROR".$stmt->error;
+			}
+		}
+	
+	function login($email, $password) {
+		
+		$notice = "";
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+			
+			$stmt = $mysqli->prepare("SELECT id, email, password, created, gender, birthdate FROM mvp_user WHERE email = ?");
+			
+			echo $mysqli->error;
+			
+			$stmt->bind_param("s", $email);
+			
+			$stmt->bind_result($id, $emailFromDb, $passwordFromDb, $created, $gender, $birthdate);
+			
+			$stmt->execute();
+			// ainult SELECTi puhul
+			if($stmt->fetch()) {
+				//oli olemas, rida käes
+				$hash = hash("sha512", $password);
+				if ($hash == $passwordFromDb){
+					echo "Kasutaja $id logis sisse";
+					
+					$_SESSION["userId"] = $id;
+					$_SESSION["userEmail"] = $emailFromDb;
+					
+					header("Location: data.php");
+					
+					} else {
+						$notice = "parool vale";
+				}
+			} else {
+				// ei olnud ühtegi rida
+				$notice = "Sellise emailiga ".$email." kasutajat ei ole olemas";
+			}
+			
+			return $notice;
+			
+		
+	}
+	function saveEvent($event, $date) {
+			
+			
+			$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+			
+			$stmt = $mysqli->prepare("INSERT INTO mvp (üritus, kuupäev) VALUE (?, ?)");
+			echo $mysqli->error;
+			
+			$stmt->bind_param("ss", $event, $date);
+			
+			if ($stmt->execute() ){
+				echo "õnnestus";
+			} else {
+				echo "ERROR".$stmt->error;
+			}
+		}
+		
+		
+	function getAllEvents (){
+			$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+			$stmt = $mysqli->prepare("SELECT id, üritus, kuupäev FROM mvp");
+			$stmt->bind_result($id, $event, $date);
+			$stmt->execute();
+			
+			$results = array();
+			// Tsükli sisu tehake nii mitu korda, mitu rida SQL lausega tuleb
+			while($stmt->fetch()) {
+				//echo $color."<br>";
+				$upcomingEvent= new StdClass();
+				$upcomingEvent->id = $id;
+				$upcomingEvent->event = $event;
+				$upcomingEvent->date = $date;
+				
+				array_push($results, $upcomingEvent);
+			}
+			
+			return $results;
+			
+			
+			
+		}
+	
+	
+?>
