031cb21eec7178c7d215cdba39f426c7a0c0aeaf
Tanel Otsa <tanelotsa@users.noreply.github.com>
2016-10-12 15:25:41 +0300

    start

---
+++ functions.php
@@ -0,0 +1,162 @@
+<?php
+	require("../../config.php");
+	
+	//see fail peab olema seotud kõigiga, kus tahame sessiooni kasutada
+	//saab kasutada nüüd $_SESSION muutujat
+	
+	session_start();
+
+
+	$database = "if16_taneotsa_4";
+	
+	function signup($email, $password) {
+		
+		//loon ühenduse 
+		
+		
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli ->prepare("INSERT INTO user_sample (email, password, gender, birthdate) VALUE(?,?,?,?)");
+		echo $mysqli->error;
+		//asendan küsimärgid
+		//iga märgikohta tuleb lisada üks täht ehk mis tüüpi muutuja on
+		//	s - string
+		//	i - int,arv
+		//  d - double
+		$stmt->bind_param("ssss", $email, $password, $_POST["gender"], $_POST["birthdate"]);
+		
+		
+		//täida käsku 
+		if($stmt->execute() ) {
+			echo "Õnnestus!";			
+		} else{
+			echo "ERROR".$stmterror;
+		}
+		
+	}
+	
+	function login ($email, $password) {
+	
+		$notice = "";
+			
+	
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli ->prepare("
+		
+					SELECT id, email, password, created, gender, birthdate
+					FROM user_sample
+					WHERE email = ? 
+					
+		");	
+		echo $mysqli->error;
+	
+		//asendan ?
+		
+		$stmt->bind_param("s", $email);
+		
+		//rea kohta tulba väärtus
+		$stmt->bind_result($id, $emailFromDb, $passwordFromDb, $created, $gender, $birthdate);
+		
+		$stmt->execute();
+		//ainult SELECT'i puhul
+		if($stmt->fetch()) {
+			//oli olemas, rida käes
+			$hash = hash("sha512", $password);
+			
+			if ($hash == $passwordFromDb) {
+				echo "Kasutaja $id logis sisse";
+				
+				$_SESSION ["userId"] = $id;
+				$_SESSION ["userEmail"] = $emailFromDb;
+				
+				header("Location: data.php");
+				
+				
+				
+				
+			} else {
+				$notice = "Parool vale !";
+			}
+		
+		} else {	
+			//ei olnud ühtegi rida
+			$notice = "Sellise e-mailiga ".$email." kasutajat ei ole olemas!";
+		}
+		
+		return $notice;
+	
+	}
+	
+	function saveShow($show,$season,$episode) {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli ->prepare("INSERT INTO sarjad (sari, hooaeg, osa) VALUE(?,?,?)");
+		echo $mysqli->error;
+		
+		$stmt->bind_param("sii", $show, $season, $episode);
+	
+		if($stmt->execute() ) {
+			echo "Õnnestus!","<br>";			
+		} else{
+			echo "ERROR".$stmterror;
+		}
+	
+	}
+	
+	function getAllShows () {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("SELECT id, sari, hooaeg, osa FROM sarjad");
+		$stmt->bind_result($id, $show, $season, $episode);
+		$stmt->execute ();
+		
+		$results = array();
+		
+		//tsükli sisu tehakse niimitu korda , mitu rida sql lausega tuleb
+		while($stmt->fetch()) {
+			
+			$tvshow = new StdClass();
+			$tvshow->id = $id;
+			$tvshow->show = $show;
+			$tvshow->season = $season;
+			$tvshow->episode = $episode;
+			
+			//echo $color."<br>";
+			array_push($results,$tvshow);
+		}
+		
+		return $results;
+	}
+
+	function cleanInput($input) {
+		
+		//eemaldab tühikud ümbert
+		$input = trim($input);
+ 		
+		//eemaldab teistpidised kaldkriipsud \\
+		$input = stripslashes($input);
+		
+		//html asendab , nt "<" muutub "&lt;"
+		$input = htmlspecialchars($input);
+		
+		return $input;
+	}
+
+
+
+
+	
+	
+	/*
+	function hello ($firstname, $lastname) {
+		return "Tere tulemast ".$firstname." ".$lastname."!";		
+	}
+
+	echo hello ("Tanel","Otsa");
+	*/
+	
+?>
---
+++ login.php
@@ -0,0 +1,260 @@
+<?php
+	
+	require("functions.php");
+	
+	//kui on sisseloginud, suunan data lehele
+	if (isset($_SESSION["userId"])) {
+		header("Location: data.php");	
+		exit();
+	}
+	
+	//var_dump($_GET);
+	//echo "<br>";
+	//var_dump($_POST);
+	$signupEmailError = "";
+	$signupEmail = "";
+	$loginEmail = "";
+	$loginEmailError = "";
+	
+	if (isset ($_POST["loginEmail"])) {
+		
+		
+		if(empty($_POST["loginEmail"])) {
+			
+			$loginEmailError = "Sisesta E-Post !";
+		
+		} else {
+			$loginEmail = $_POST["loginEmail"];
+		}
+	
+	}
+	
+	
+	$loginPasswordError = "";
+	
+	if (isset ($_POST["loginPassword"])) {
+		
+		
+		if(empty($_POST["loginPassword"])) {
+			
+			$loginPasswordError = "Sisesta Parool !";
+		
+		} 
+	
+	}
+		
+	
+	//kas keegi vajutas nuppu 
+	
+	if (isset ($_POST["signupEmail"])) {
+		
+		//on olemas
+		//kas email on olemas
+		if(empty($_POST["signupEmail"])) {
+			
+			//on tühi
+			$signupEmailError = "Väli on kohustuslik!";
+		
+		} else {
+			$signupEmail = $_POST["signupEmail"];
+		}
+	
+	}
+	
+	//kas epost on tühi
+	
+	$signupPasswordError = "";
+	
+	if (isset ($_POST["signupPassword"])) {
+		
+		if(empty($_POST["signupPassword"])) {
+			
+			$signupPasswordError = "Väli on kohustuslik!";
+			
+		} else { 
+		
+			if (strlen($_POST["signupPassword"]) < 8 ) {
+				
+				$signupPasswordError = "* Parool peab olema vähemalt kaheksa märki!";
+				}
+		
+			}	
+		
+	}
+	
+	
+	$birthdateError = "";
+	
+	if (isset ($_POST["birthdate"])) {
+		
+		
+		if(empty($_POST["birthdate"])) {
+			
+			//on tühi
+			$birthdateError = "Sisesta sünnikuupäev!";
+		
+		}
+	
+	}
+	
+	$genderError = "";
+	
+	if (isset ($_POST["gender"])) {
+		
+		
+		if(empty($_POST["gender"])) {
+			
+			$genderError = "Sisesta sugu !";
+		
+		}
+	
+	}
+	
+		$gender = "Mees";
+	
+	if (isset ($_POST["gender"])) {
+		if (empty ($_POST["gender"])) {
+			$genderError = "* Väli on kohustuslik!";
+		} else {
+			$gender = $_POST["gender"];
+		}
+		
+	} 
+	
+	if ( $signupEmailError == "" &&
+		 $signupPasswordError == "" &&
+		 $birthdateError == "" &&
+		 $genderError == "" &&
+		 
+		 isset($_POST["signupEmail"]) &&
+		 isset($_POST["signupPassword"]) &&
+		 isset($_POST["birthdate"]) &&
+		 isset($_POST["gender"])
+		 
+		) {
+	
+		//kõik olemas, vigu polnud
+		echo "SALVESTAN...<br>";
+		echo "email ".$signupEmail."<br>";
+		echo "parool ".$_POST["signupPassword"]."<br>";
+		echo "sünnikuupäev ".$_POST["birthdate"]."<br>";
+		echo "sugu " .$_POST["gender"]. "<br>";
+		
+		$password = hash("sha512",$_POST["signupPassword"]);
+	
+		//echo $password ;
+		
+		signup($signupEmail, $password);
+		
+		
+		}
+		
+		
+		
+		$notice = "";
+		//kas kasutaja tahab sisse logida
+		if ( isset($_POST["loginEmail"]) &&
+			 isset($_POST["loginPassword"]) &&
+			 !empty($_POST["loginEmail"]) &&
+			 !empty($_POST["loginPassword"]) 
+		
+		) {
+			$notice = login($_POST["loginEmail"], $_POST["loginPassword"]);
+		
+		} 
+			 
+			 
+			 
+?>
+	
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sisselogimise leht</title>
+	</head>
+<body>
+
+	<h1>Logi Sisse</h1>
+	<p> <?=$notice;?> </p>
+	<form method="POST">
+	
+		<label>E-Post:</label> 
+		
+		<br>
+		
+		<input name="loginEmail" type = "email" value="<?=$loginEmail;?>"> <?php echo $loginEmailError ; ?>
+		
+		<br><br>
+		
+		<label>Parool:</label>
+		
+		<br>
+		
+		<input name="loginPassword" type = "password" > <?php echo $loginPasswordError ; ?>
+	
+		<br><br>
+		
+		<input type = "submit" value = "LOGI SISSE" >
+		
+	</form>
+	
+	<br><br>
+	<h1>Loo kasutaja</h1>
+	<form method="POST">
+	
+		<label>E-Post:</label> 
+		
+		<br>
+		
+		<input name="signupEmail" type = "email" value="<?=$signupEmail;?>"> <?php echo $signupEmailError ; ?>
+		
+		<br><br>
+		
+		
+		<label>Parool:</label>
+		
+		<br>
+		
+		<input name="signupPassword" type = "password" > <?php echo $signupPasswordError; ?>
+	
+		<br><br>
+		
+		
+		<label>Sünnikuupäev:</label>
+		
+		<br>
+		
+		<input name="birthdate" type = "date" > <?php echo $birthdateError; ?>
+	
+		<br><br>
+		
+		
+		<label>Sugu:</label>
+		
+		<br>
+		
+		<?php if ($gender == "Mees") { ?>
+			<input name="gender" type = "radio" value ="Mees" checked >	Mees
+		<?php } else { ?>
+			<input name="gender" type = "radio" value ="Mees" >	Mees
+		<?php } ?>
+		
+		<br>
+		
+		<?php if ($gender == "Naine") { ?>
+			<input name="gender" type = "radio" value ="Naine" checked > Naine
+		<?php } else { ?>
+			<input name="gender" type = "radio" value ="Naine" > Naine
+		<?php } ?>
+		
+		<br><br>
+		
+		<input type = "submit" value = "LOO KASUTAJA" >
+		
+			
+		
+		
+	</form>
+	
+	</body>
+</html>
